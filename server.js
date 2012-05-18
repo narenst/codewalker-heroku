17425c2c4cef7b8464511fc0c6fb6f4f09ea20ae
Narendran Thiagarajan <narenst@gmail.com>
2012-05-18 00:34:01 -0700

    cleaning up the client server communication protocol

--- server.js
+++ server.js
@@ -14,11 +14,98 @@ app.all('/*', function (req, res, next) {
   next();
 });
 
-var masterSelected = false;
-var master = null;
+var masterSelected = {};
+
+var master = {};
+var totalParticipants = {};
+var socketToRoomMap = {};
+
+function sendSuccess(socket, operation) {
+   console.log("sending success to : " + socket.id + " for operation : " + operation);
+   socket.emit('success', operation);
+}
+
+function sendFailure(socket, operation, reason) {
+   console.log("sending failure to : " + socket.id + " for operation : " + operation);
+   socket.emit('failure', operation, reason);
+}
+
+function sendMasterUpdate(roomId) {
+   console.log("sending master update for room : " + roomId);
+   ret = {};
+   ret["participants"] = totalParticipants.hasOwnProperty(roomId)?
+                           totalParticipants[roomId]:
+                           0;
+   ret["isMasterSelected"] = master.hasOwnProperty(roomId);
+
+   console.log("participants : " + ret["participants"] + 
+                  ", isMasterSelected : " + ret["isMasterSelected"]);
+   io.sockets.in(roomId).emit('masterUpdate', JSON.stringify(ret));
+}
+
+function addParticipant(roomId) {
+   if(totalParticipants.hasOwnProperty(roomId)) {
+      totalParticipants[roomId] += 1;
+   } else {
+      totalParticipants[roomId] = 1;
+   }
+}
+
+function removeParticipant(roomId) {
+   if(totalParticipants.hasOwnProperty(roomId)) {
+      totalParticipants[roomId] -= 1;
+   }
+}
+
+function joinRoom(socket, roomId) {
+   if (socketToRoomMap.hasOwnProperty(socket.id)) {
+      sendFailure(socket, "join", "already part of a room : " + 
+                     socketToRoomMap[socket.id]);
+      return;
+   }
+   socketToRoomMap[socket.id] = roomId;
+   socket.join(roomId);
+   sendSuccess(socket, "join");
+   addParticipant(roomId);
+   sendMasterUpdate(roomId);
+}
+
+function leaveRoom(socket) {
+   if (!socketToRoomMap.hasOwnProperty(socket.id)) {
+      sendFailure(socket, "leave", "has not joined any room before");
+   } else {
+      roomId = socketToRoomMap[socket.id];
+      delete socketToRoomMap[socket.id];
+
+      //clear master if this socket was the master
+      if(master.hasOwnProperty(roomId) &&
+         master[roomId] == socket) {
+         delete master[roomId];
+      }
+      socket.leave(roomId);
+      sendSuccess(socket, "leave");
+      removeParticipant(roomId);
+      sendMasterUpdate(roomId);
+   }
+}
 
 //on new connection
 io.sockets.on('connection', function (socket) {
+
+   sendSuccess(socket, "connect");
+
+   //when socket joins a room
+   socket.on('join', function (roomId) {
+      console.log("received join from client for room : " + roomId);
+      joinRoom(socket, roomId);
+   });
+
+   //when socket joins a room
+   socket.on('leave', function () {
+      console.log("received leave room from client");
+      leaveRoom(socket);
+   });
+
 
    // when the master sends 'sendupdate', this listens and executes
    socket.on('sendupdate', function (data) {
@@ -59,12 +146,12 @@ io.sockets.on('connection', function (socket) {
    }   
 
    //new client joined
-   broadcastMasterStatus();
+   //broadcastMasterStatus();
 
    // when the user disconnects.. perform this
    socket.on('disconnect', function(){
-      if (socket == master) {
-         clearMaster();
+      if (socketToRoomMap.hasOwnProperty(socket)) {
+         leaveRoom(socket);
       }
    });
 });
